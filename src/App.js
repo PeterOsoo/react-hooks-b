894cc82409c9df006d945e41c9a86d0a47d03f9d
Peter Osoo <posoo617@gmail.com>
2023-04-07 19:35:42 +0300

    useState hook - counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react"
+import "./App.css"
+import UseState from "./components/UseState/UseState"
 
 const App = () => {
-	return <div>React Hooks</div>
+	return (
+		<div>
+			<h1>React Hooks </h1>
+			<UseState />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/UseState/UseState.js
@@ -0,0 +1,12 @@
+import React from "react"
+import HookCounter from "./HookCounter"
+
+const UseState = () => {
+	return (
+		<div>
+			<HookCounter />
+		</div>
+	)
+}
+
+export default UseState

18ea8c120cebecd3a7a56f3c61144f683c8381ae
Peter Osoo <posoo617@gmail.com>
2023-04-08 13:13:10 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react"
 import "./App.css"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 import UseState from "./components/UseState/UseState"
 
 const App = () => {
 	return (
-		<div>
+		<Router>
 			<h1>React Hooks </h1>
 			<UseState />
-		</div>
+			<Routes>
+				<Route
+					path="/"
+					exact
+					strict
+					render={() => {
+						return <p> Welcome home</p>
+					}}
+				/>
+			</Routes>
+		</Router>
 	)
 }
 

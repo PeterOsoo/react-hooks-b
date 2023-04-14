9975fe2a0fdceae4695981616af1d84abd4ace1e
Peter Osoo <posoo617@gmail.com>
2023-04-14 14:56:40 +0300

    display pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from "react"
 import "./App.css"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 import UseState from "./components/UseState/UseState"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
 const App = () => {
 	return (
-		<Router>
+		<>
 			<h1>React Hooks </h1>
-			<UseState />
 			<Routes>
-				<Route
-					path="/"
-					exact
-					strict
-					render={() => {
-						return <p> Welcome home</p>
-					}}
-				/>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/use-state" element={<UseState />} />
 			</Routes>
-		</Router>
+		</>
 	)
 }
 

9dd36cb16ecb6a154ad38cc0f3dd78c086938dbb
Peter Osoo <posoo617@gmail.com>
2023-04-17 16:31:04 +0300

    useEffect after render

---
+++ src/components/UseEffect/HookCounterOne.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react"
+
+const HookCounterOne = () => {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		document.title = `Clicked ${count} times`
+	})
+
+	return (
+		<div>
+			<button onClick={() => setCount(count + 1)}>
+				Clicked {count} times{" "}
+			</button>
+		</div>
+	)
+}
+
+export default HookCounterOne
---
+++ src/components/UseEffect/UseEffect.js
@@ -0,0 +1,12 @@
+import HooksCounterOne from "./HookCounterOne"
+
+const UseEffect = () => {
+	return (
+		<div>
+			<h3>Use Effect Hooks</h3>
+			<HooksCounterOne />
+		</div>
+	)
+}
+
+export default UseEffect

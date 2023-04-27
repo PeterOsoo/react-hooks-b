3198d595f99f7ccafae65f0caa8877cbc2ea7900
Peter Osoo <posoo617@gmail.com>
2023-04-27 16:25:35 +0300

    useEffect page

---
+++ src/pages/UseEffect.js
@@ -0,0 +1,16 @@
+import HooksCounterOne from "../components/UseEffect/HookCounterOne"
+// import HookMouse from "./HookMouse"
+import MouseContainer from "../components/UseEffect/MouseContainer"
+
+const UseEffect = () => {
+	return (
+		<div>
+			<h3>Use Effect Hooks</h3>
+			<HooksCounterOne />
+			{/* <HookMouse /> */}
+			<MouseContainer />
+		</div>
+	)
+}
+
+export default UseEffect

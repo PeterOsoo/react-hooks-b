bf0472768823b290b8f3f014ebf792e0ddcd3c9e
Peter Osoo <posoo617@gmail.com>
2023-05-18 11:46:42 +0300

    useReducer - simple state and action

---
+++ src/components/UseReducer/CounterOne.js
@@ -0,0 +1,31 @@
+import { useReducer } from "react"
+
+const initialState = 0
+const reducer = (state, action) => {
+	switch (action) {
+		case "increment":
+			return state + 1
+		case "decrement":
+			return state - 1
+		case "reset":
+			return initialState
+		default:
+			return state
+	}
+}
+
+const CounterOne = () => {
+	const [count, dispatch] = useReducer(reducer, initialState)
+
+	return (
+		<div>
+			<h4>useReducer - simple state and action</h4>
+			<p>Count {count}</p>
+			<button onClick={() => dispatch("increment")}>Increment</button>
+			<button onClick={() => dispatch("decrement")}>Decrement</button>
+			<button onClick={() => dispatch("reset")}>Reset</button>
+		</div>
+	)
+}
+
+export default CounterOne
--- src/pages/UseReducer.js
+++ src/pages/UseReducer.js
@@ -1,7 +1,14 @@
 import React from "react"
+import CounterOne from "../components/UseReducer/CounterOne"
 
 const UseReducer = () => {
-	return <div>UseReducer</div>
+	return (
+		<div>
+			UseReducer Hooks
+			<h3>UseReducer Hooks</h3>
+			<CounterOne />
+		</div>
+	)
 }
 
 export default UseReducer
